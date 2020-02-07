8dcd8212ad4f9fd5c45a9ba6b37856ef9e4c421d
leoboyerbx <leo.boyer@outlook.com>
2020-02-07 23:05:00 +0100

    logo update

--- py/tray.py
+++ py/tray.py
@@ -5,11 +5,34 @@ gi.require_version('AppIndicator3', '0.1')
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk as gtk, AppIndicator3 as appindicator
 
-def main():
-    indicator = appindicator.Indicator.new_with_path("customtray", "all-stopped", appindicator.IndicatorCategory.APPLICATION_STATUS)
-    indicator.set_status(appindicator.IndicatorStatus.ACTIVE)
-    indicator.set_menu(menu())
-    gtk.main()
+
+_dir_path = os.path.dirname(os.path.realpath(__file__))
+
+class Indicator(object):    
+    def __init__(self, menu):
+
+        self.ind = appindicator.Indicator.new (
+                          "thetool",
+                          "",
+                          appindicator.IndicatorCategory.APPLICATION_STATUS)
+        self.ind.set_status (appindicator.IndicatorStatus.ACTIVE)
+        self.ind.set_icon(os.path.join(_dir_path, 'img', 'all-stopped.png'))
+        # self.ind.set_attention_icon (os.path.join(_curr_dir, 'img', 'tools-active.png'))
+        self.ind.set_menu(menu)
+
+    def set_attention(self, attention):
+        if attention:
+            self.ind.set_status (appindicator.IndicatorStatus.ATTENTION)
+        else:
+            self.ind.set_status (appindicator.IndicatorStatus.ACTIVE)
+    def setState(self, state):
+        if (state == 'running'):
+            self.ind.set_icon(os.path.join(_dir_path, 'img', 'all-started.png'))
+        elif (state == 'partial'):
+            self.ind.set_icon(os.path.join(_dir_path, 'img', 'one-started.png'))
+        else:
+            self.ind.set_icon(os.path.join(_dir_path, 'img', 'all-stopped.png'))
+
 
 def menu():
     menu = gtk.Menu()
@@ -25,8 +48,15 @@ def menu():
     return menu
 
 
+def main():
+    global tray
+    tray = Indicator(menu())
+    gtk.main()
+
+
 def note(_):
-    os.system("gedit $HOME/Documents/notes.txt")
+    global tray
+    tray.setState('running')
 def quit(_):
     gtk.main_quit()
 if __name__ == "__main__":
