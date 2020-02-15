419e84e7b674e7369d2fb727897e975018e0bc9a
leoboyerbx <leo.boyer@outlook.com>
2020-02-15 18:57:37 +0100

    WIP on add form

--- py/vhost_manager.py
+++ py/vhost_manager.py
@@ -8,9 +8,25 @@ class AddForm (Gtk.Window):
 
     def __init__(self):
         Gtk.Window.__init__(self, title="Add Apache Virtual Host")
-        self.button = Gtk.Button(label="Click Here")
-        self.button.connect("clicked", self.cancel)
-        self.add(self.button)
+
+        self.vBox = Gtk.Box(spacing=5, orientation=1)
+        self.add(self.vBox)
+
+        self.fieldBox1 = Gtk.Box(spacing=5)
+        self.vBox.pack_start(self.fieldBox1, True, True, 0)
+
+        nameFieldLabel
+
+        self.buttonsBox = Gtk.Box(spacing=5)
+        self.vBox.pack_start(self.buttonsBox, True, True, 0)
+
+        self.buttonSave = Gtk.Button(label="Save")
+        self.buttonSave.connect("clicked", self.cancel)
+        self.buttonsBox.pack_start(self.buttonSave, True, True, 0)
+
+        self.buttonSave = Gtk.Button(label="Cancel")
+        self.buttonSave.connect("clicked", self.cancel)
+        self.buttonsBox.pack_start(self.buttonSave, True, True, 0)
 
     def save(self, widget):
         print("Hello World")
